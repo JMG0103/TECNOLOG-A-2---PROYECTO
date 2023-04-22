ab2e8f0e1ee63d861a409917bd1d0876172b5dad
Jesús García <122702898+JMG0103@users.noreply.github.com>
2023-04-22 16:15:39 -0400

    Revert "Create Dropdownmodify.js"
    This reverts commit 23d6a11c428a27ad5f92183dbcc2490b9636d0dd.

--- Changes/Dropdownmodify.js
+++
@@ -1,7 +0,0 @@
-function show2(value) {
-    document.querySelector(".text-box2").value = value;
-  }
-  let dropdown2 = document.querySelector(".dropdown2");
-  dropdown2.onclick = function () {
-    dropdown2.classList.toggle("active");
-  };
